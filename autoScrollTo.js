a1422d3fbead0c518a7acdf0dfb92e24f0da1dda
Richard Hardesty <richardhardesty38@gmail.com>
2014-10-26 15:36:15 +0000

    animated scrollTo

---
+++ autoScrollTo.js
@@ -0,0 +1,39 @@
+var scrollY = 0;
+var distance = 40;
+var speed = 25;
+        
+function autoScrollTo(el) {
+    "use strict";
+    var currentY = window.pageYOffset;
+    var targetY = document.getElementById(el).offsetTop;
+    var bodyHeight = document.body.offsetHeight;
+    var yPos = currentY + window.innerHeight;
+            
+    var animator = setTimeout('autoScrollTo(\'' + el + '\')', speed);
+            
+    if (yPos > bodyHeight) {
+        clearTimeout(animator);
+    } else {
+        if (currentY < targetY - distance) {
+            scrollY = currentY + distance;
+            window.scroll(0, scrollY);
+        } else {
+            clearTimeout(animator);
+        }
+    }
+} //End autoScrollTo
+        
+function resetScroller(el) {
+    "use strict";
+    var currentY = window.pageYOffset;
+    var targetY = document.getElementById(el).offsetTop;
+
+    var animator = setTimeout('resetScroller(\'' + el + '\')', speed);// maybe have own variable
+            
+    if (currentY > targetY) {
+        scrollY = currentY - distance;
+        window.scroll(0, scrollY);
+    } else {
+        clearTimeout(animator);
+    }
+}
